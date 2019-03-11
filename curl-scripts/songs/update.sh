7a81e75009edd50cfa99d559ea721d8a941d75f4
hjsharkey23 <hjsharkey23@gmail.com>
2019-03-10 20:50:53 -0400

    Add update and get curl scripts
    -Add curl script to get all songs
    -Add curl script for updating song by ID

---
+++ curl-scripts/songs/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl "http://localhost:4741/songs/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --data '{
+    "song": {
+      "artist": "'"${ARTIST}"'",
+      "tuning": "'"${TUNING}"'",
+      "capo": "'"${CAPO}"'",
+      "chords": "'"${CHORDS}"'",
+      "title": "'"${TITLE}"'"
+    }
+  }'
+
+echo
